1b765be52f599fb0bc3e8cd7182b9a3ebaef8354
Brian Huffman <huffman@galois.com>
2015-12-01 10:36:43 -0800

    Add Show instances for parser datatypes

--- src/BON/Parser/AST.hs
+++ src/BON/Parser/AST.hs
@@ -1,9 +1,11 @@
 module BON.Parser.AST where
 
 data Comment = MkComment [String]
+  deriving Show
 
 data BonSourceFile
   = MkBonSourceFile [SpecificationElement] (Maybe Indexing)
+  deriving Show
 
 data SpecificationElement
   = InformalChart InformalChart
@@ -11,6 +13,7 @@ data SpecificationElement
   | StaticDiagram StaticDiagram
   | DynamicDiagram DynamicDiagram
   -- | NotationalTuning _
+  deriving Show
 
 data InformalChart
   = SystemChart ClusterChart
@@ -19,6 +22,7 @@ data InformalChart
   | EventChart EventChart
   | ScenarioChart ScenarioChart
   | CreationChart CreationChart
+  deriving Show
 
 data ClassDictionary
   = MkClassDictionary
@@ -27,9 +31,11 @@ data ClassDictionary
     (Maybe Indexing) -- ^ indexing
     (Maybe String) -- ^ explanation
     (Maybe String) -- ^ part
+  deriving Show
 
 data DictionaryEntry
   = DictionaryEntry ClassName [String] String
+  deriving Show
 
 data ClusterChart
   = MkClusterChart
@@ -40,24 +46,29 @@ data ClusterChart
     (Maybe Indexing) -- ^ indexing
     (Maybe String) -- ^ explanation
     (Maybe String) -- ^ part
+  deriving Show
 
 data Indexing
   = MkIndexing [IndexClause]
+  deriving Show
 
 data ClusterEntry
   = MkClusterEntry
     String -- ^ cluster_name
     String -- ^ description
+  deriving Show
 
 data IndexClause
   = MkIndexClause
     String -- ^ identifier
     [String] -- ^ index_term_list
+  deriving Show
 
 data ClassEntry
   = MkClassEntry
     ClassName
     String -- ^ description
+  deriving Show
 
 data ClassChart
   = MkClassChart
@@ -69,6 +80,7 @@ data ClassChart
     (Maybe Indexing) -- ^ indexing
     (Maybe String) -- ^ explanation
     (Maybe String) -- ^ part
+  deriving Show
 
 type ClassName = String
 
@@ -80,13 +92,16 @@ data EventChart
     (Maybe Indexing)
     (Maybe String) -- ^ explanation
     (Maybe String) -- ^ part
+  deriving Show
 
 data Direction = Incoming | Outgoing
+  deriving Show
 
 data EventEntry
   = MkEventEntry
     String -- ^ 'event' description
     [String] -- ^ 'involves' class_or_cluster_name_list
+  deriving Show
 
 data ScenarioChart
   = MkScenarioChart
@@ -95,11 +110,13 @@ data ScenarioChart
     (Maybe Indexing)
     (Maybe String) -- ^ explanation
     (Maybe String) -- ^ part
+  deriving Show
 
 data ScenarioEntry
   = MkScenarioEntry
     String -- ^ manifest_string
     String -- ^ description
+  deriving Show
 
 data CreationChart
   = MkCreationChart
@@ -108,22 +125,26 @@ data CreationChart
     (Maybe Indexing)
     (Maybe String) -- ^ explanation
     (Maybe String) -- ^ part
+  deriving Show
 
 data CreationEntry
   = MkCreationEntry
     String -- ^ class_name
     [String] -- ^ class_or_cluster_name_list
+  deriving Show
 
 data StaticDiagram
   = MkStaticDiagram
     [StaticComponent]
     (Maybe String) -- ^ extended_id
     Comment
+  deriving Show
 
 data StaticComponent
   = Cluster Cluster
   | Class Class
   | StaticRelation StaticRelation
+  deriving Show
 
 data Cluster
   = MkCluster
@@ -131,6 +152,7 @@ data Cluster
     [StaticComponent]
     Bool -- ^ reused?
     Comment
+  deriving Show
 
 data Class
   = MkClass
@@ -142,15 +164,18 @@ data Class
     Bool -- ^ persistent
     Bool -- ^ interfaced
     Comment
+  deriving Show
 
 data ClassMod
   = ROOT
   | DEFERRED
   | EFFECTIVE
+  deriving Show
 
 data StaticRelation
   = InheritanceRelation InheritanceRelation
   | ClientRelation ClientRelation
+  deriving Show
 
 data InheritanceRelation
   = MkInheritanceRelation
@@ -158,6 +183,7 @@ data InheritanceRelation
     StaticRef -- ^ parent
     (Maybe Integer) -- ^ multiplicity
     (Maybe String) -- ^ semantic_label
+  deriving Show
 
 data ClientRelation
   = MkClientRelation
@@ -166,55 +192,68 @@ data ClientRelation
     (Maybe ClientEntityExpression) -- ^ client_entities
     (Maybe TypeMark) -- ^ type_mark
     (Maybe String) -- ^ semantic_label
+  deriving Show
 
 data ClientEntityExpression
   = ClientEntityList [ClientEntity]
   | Multiplicity Integer
+  deriving Show
 
 data ClientEntity
   = SupplierIndirection SupplierIndirection
   | ParentIndirection ParentIndirection
+  deriving Show
 
 data SupplierIndirection
   = MkSupplierIndirection
     (Maybe IndirectionFeaturePart)
     GenericIndirection
+  deriving Show
 
 data IndirectionFeaturePart
   = FeatureName FeatureName
   | IndirectionFeatureList IndirectionFeatureList
+  deriving Show
 
 data IndirectionFeatureList
   = MkIndirectionFeatureList [FeatureName]
+  deriving Show
 
 data ParentIndirection
   = MkParentIndirection GenericIndirection
+  deriving Show
 
 data GenericIndirection
   = MkGenericIndirection IndirectionElement
+  deriving Show
 
 data NamedIndirection
   = MkNamedIndirection
     ClassName
     [IndirectionElement]
+  deriving Show
 
 data IndirectionElement
   = CompactedIndirectionElementImpl
   | NamedIndirection NamedIndirection
   | ClassName ClassName
+  deriving Show
 
 data TypeMark
   = TypeMarkHASTYPE
   | TypeMarkAGGREGATE
   | TypeMarkSHAREDMARK Integer
+  deriving Show
 
 data StaticRef
   = MkStaticRef
     [StaticRefPart] -- ^ cluster_prefix
     StaticRefPart -- ^ static_component_name
+  deriving Show
 
 data StaticRefPart
   = MkStaticRefPart String
+  deriving Show
 
 data ClassInterface
   = MkClassInterface
@@ -222,6 +261,7 @@ data ClassInterface
     [Type] -- ^ parent_class_list
     [Expression] -- ^ class_invariant
     (Maybe Indexing) -- ^ indexing
+  deriving Show
 
 data Expression
   = Quantification Quantification
@@ -230,17 +270,20 @@ data Expression
   | Constant Constant
   | CallExp Expression UnqualifiedCall
   | UnqualifiedCall UnqualifiedCall
+  deriving Show
 
 data Type
   = MkType
     ClassName
     [Type] -- ^ actual_generics
+  deriving Show
 
 data Feature
   = MkFeature
     [FeatureSpecification]
     [ClassName] -- ^ selective_export
     Comment
+  deriving Show
 
 data FeatureSpecification
   = MkFeatureSpecification
@@ -251,36 +294,44 @@ data FeatureSpecification
     (Maybe HasType)
     (Maybe RenameClause)
     Comment
+  deriving Show
 
 data FeatureSpecificationModifier
   = FeatureSpecDEFERRED
   | FeatureSpecEFFECTIVE
   | FeatureSpecREDEFINED
   | FeatureSpecNONE
+  deriving Show
 
 data HasType
   = MkHasType TypeMark Type
+  deriving Show
 
 data ContractClause
   = MkContractClause
     [Expression] -- ^ preconditions
     [Expression] -- ^ postconditions
+  deriving Show
 
 data FeatureName
   = MkFeatureName String
+  deriving Show
 
 data RenameClause
   = MkRenameClause ClassName FeatureName
+  deriving Show
 
 data FeatureArgument
   = MkFeatureArgument
     String -- ^ identifier
     Type
+  deriving Show
 
 data FormalGeneric
   = MkFormalGeneric
     String -- ^ formal_generic_name
     (Maybe Type) -- ^ class_type
+  deriving Show
 
 data Quantification
   = MkQuantification
@@ -288,43 +339,54 @@ data Quantification
     [VariableRange] -- ^ range_expression
     (Maybe Expression) -- ^ restriction
     Expression -- ^ proposition
+  deriving Show
 
 data Quantifier = FORALL | EXISTS
+  deriving Show
 
 data VariableRange
   = MemberRange MemberRange
   | TypeRange TypeRange
+  deriving Show
 
 data MemberRange
   = MkMemberRange [String] Expression
+  deriving Show
 
 data TypeRange
   = MkTypeRange [String] Type
+  deriving Show
 
 data UnqualifiedCall
   = MkUnqualifiedCall
     String -- ^ identifier
     [Expression] -- ^ actual_arguments
+  deriving Show
 
 data EnumerationElement
   = Expression Expression
   | Interval Interval
+  deriving Show
 
 data Interval
   = IntegerInterval IntegerInterval
   | CharacterInterval CharacterInterval
+  deriving Show
 
 data IntegerInterval
   = MkIntegerInterval Integer Integer
+  deriving Show
 
 data CharacterInterval
   = MkCharacterInterval Char Char
+  deriving Show
 
 data Constant
   = ManifestConstant ManifestConstant
   | CurrentConstant
   | VoidConstant
   | ResultConstant
+  deriving Show
 
 data ManifestConstant
   = BooleanConstant Bool
@@ -333,12 +395,14 @@ data ManifestConstant
   | RealConstant Double
   | StringConstant String
   | SetConstant [EnumerationElement]
+  deriving Show
 
 data DynamicDiagram
   = MkDynamicDiagram
     [DynamicComponent] -- ^ components
     (Maybe String) -- ^ extended_id
     Comment
+  deriving Show
 
 data DynamicComponent
   = ScenarioDescription ScenarioDescription
@@ -346,17 +410,20 @@ data DynamicComponent
   | ObjectStack ObjectStack
   | ObjectInstance ObjectInstance
     -- TODO: message_relation
+  deriving Show
 
 data ScenarioDescription
   = MkScenarioDescription
     String -- ^ scenario_name
     [LabelledAction] -- ^ labelled_actions
     Comment
+  deriving Show
 
 data LabelledAction
   = MkLabelledAction
     String -- ^ action_label
     String -- ^ action_description
+  deriving Show
 
 data ObjectGroup
   = MkObjectGroup
@@ -364,21 +431,25 @@ data ObjectGroup
     String -- ^ group_name
     [DynamicComponent]
     Comment
+  deriving Show
 
 data ObjectStack
   = MkObjectStack
     ObjectName
     Comment
+  deriving Show
 
 data ObjectInstance
   = MkObjectInstance
     ObjectName
     Comment
+  deriving Show
 
 data ObjectName
   = MkObjectName
     ClassName
     (Maybe String) -- ^ extended_id
+  deriving Show
 
 data BinaryOp
   = Add | Sub
@@ -389,7 +460,9 @@ data BinaryOp
   | Mul | Div | IntDiv
   | Mod | Pow
   | Equiv | Implies
+  deriving Show
 
 data UnaryOp
   = UAdd | USub
   | DELTA | OLD | NOT
+  deriving Show
